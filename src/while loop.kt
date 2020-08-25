d9f7aa66653832941e3c03cb8652b1a198ceb1b0
paulndungu <p.ndungu.kara@gmail.com>
2020-08-25 12:56:08 +0300

    wrote down the code for the while loop file

---
+++ src/while loop.kt
@@ -0,0 +1,13 @@
+fun main (){
+
+    var count=1
+        while(count<=5) {
+            if (count==3) {
+                println("number= $count")
+            }
+//            number=number*10
+         count++
+        }
+    println("end of thy loop")
+
+}
